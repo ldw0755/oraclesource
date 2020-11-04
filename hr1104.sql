17bf3d2730a563bf8eb3e34057dc14ba2dde4aa6
LDW <ldw0755@naver.com>
2020-11-04 18:22:19 +0900

    20/11/04 real final

--- hr1104.sql
+++ hr1104.sql
@@ -31,9 +31,11 @@ DESC employees;
 --20번 및 50번 부서에서 근무하는 모든 사원들의 LAST_NAME 및 부서 번호를 알파벳 순으로 조회
 SELECT last_name, department_id FROM employees WHERE department_id IN(20, 50) ORDER BY last_name;
 --커미션을 버는 모든 사원들의 LAST_NAME, 연봉, 커미션 조회. 단, 연봉의 역순 및 커미션의 역순으로 출력
-SELECT last_name, salary, commission_pct FROM employees WHERE commission_pct >0 ORDER BY salary DESC, commission_pct DESC;
+SELECT last_name, salary, commission_pct FROM employees WHERE commission_pct <>0 ORDER BY salary DESC, commission_pct DESC;
 --연봉이 2500, 3500, 7000이 아니며 직업이 SA_REP이거나 ST_CLERK인 사람들 조회
-SELECT * FROM employees WHERE salary NOT IN(2500, 3500, 7000) and job_id IN('sa_rep', 'st_clerk');
+SELECT * FROM employees WHERE salary NOT IN(2500, 3500, 7000) AND job_id IN('SA_REP', 'ST_CLERK');
+SELECT * FROM employees WHERE salary NOT IN(2500, 3500, 7000) AND job_id IN('sa_rep', 'st_clerk');
+SELECT * FROM employees;
 select distinct job_id from employees;
 --2008/02/02 ~ 2008/05/01 사이에 고용된 사원의 LAST_NAME, 사원번호, 고용일자 조회.
 --단, 고용일자의 내림차순 정렬
