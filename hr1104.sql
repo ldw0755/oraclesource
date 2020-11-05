9d8f4ab21a4a07b26e0136232365d1f338ad27c3
LDW <ldw0755@naver.com>
2020-11-05 18:11:38 +0900

    oracle function

--- hr1104.sql
+++ hr1104.sql
@@ -44,3 +44,75 @@ SELECT last_name, employee_id, hire_date FROM employees WHERE hire_date >= '08/0
 --2004년도에 고용된 모든 사람들의 LAST_NAME 및 고용일을 조회
 --단, 입사일 기준 오름차순 정렬
 SELECT last_name, hire_date FROM employees WHERE hire_date >= '04/01/01' ORDER BY hire_date;
+SELECT last_name, hire_date FROM employees WHERE hire_date >= '2004-01-01' ORDER BY hire_date;
+SELECT last_name, hire_date FROM employees WHERE hire_date >= '2004.01.01' AND hire_date <= '04-12-31' ORDER BY hire_date;
+
+--연봉 5000~12000 범위, 20 or 50번 부서에 근무하는 사람들의 연봉 조회, 오름차순으로
+select * from employees;
+SELECT last_name, salary 
+FROM employees 
+WHERE (salary BETWEEN 5000 AND 12000) AND (department_id IN(20, 50)) 
+ORDER BY salary;
+
+--2004년도 고용된 모든 사람들의 last_name, 및 고용일 조회, 입사일 기준 오름차순 정리
+desc employees;
+SELECT last_name, hire_date 
+FROM employees 
+WHERE hire_date BETWEEN '2004-01-01' AND '2004-12-31'
+ORDER BY hire_date;
+
+--연봉 5000~12000 범위 이외, last_name, salary 조회
+SELECT last_name, salary
+FROM employees
+WHERE salary NOT BETWEEN 5000 AND 12000
+ORDER BY salary;
+
+--LIKE 연습
+--2004년도 고용된 모든 사람들의 last_name, 및 고용일 조회, 입사일 기준 오름차순 정리
+SELECT last_name, hire_date 
+FROM employees 
+WHERE hire_date LIKE '2004%' OR hire_date LIKE '04%' 
+ORDER BY hire_date;
+--LAST_NAME 에 u가 포함되는 사원들의 사번 및 last_name 조회
+DESC employees;
+SELECT employee_id, last_name
+FROM employees
+WHERE last_name LIKE '%u%';
+--last_name에 네번째 글자가 a인 사람들의 last_name 조회
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '___a%';
+-- last_name에 a 혹은 e글자가 있는 사원들의 last_name을 조회하여 last_name의 오름차순 정렬
+SELECT last_name
+FROM employees
+WHERE (last_name LIKE '%a%') OR (last_name LIKE '%e%')
+ORDER BY last_name;
+
+--IS NULL 연습
+--매니저가 없는 사람들의 LAST_NAME, JOB_ID 조회
+DESC employees;
+SELECT last_name, job_id
+FROM employees
+WHERE manager_id IS NULL;
+--ST_CLERK인 JOB_ID를 가진 사원이 없는 부서 ID 조회, 부서 번호가 NULL인 값 제외
+
+SELECT DISTINCT DEPARTMENT_ID
+FROM EMPLOYEES
+WHERE (DEPARTMENT_ID IS NOT NULL) AND (JOB_ID NOT IN('ST_CLERK'))
+ORDER BY DEPARTMENT_ID;
+
+SELECT DISTINCT DEPARTMENT_ID, JOB_ID FROM EMPLOYEES
+ORDER BY DEPARTMENT_ID;
+
+--COMMISSSION_PCT가 NULL이 아닌 사원 중
+--COMMISSION = SALALY*COMMISSION_PCT를 구하여
+--EMPLOYEE_ID, FIRST_NAME, JOB_ID와 같이 조회
+SELECT employee_id, first_name, job_id, (salary*commission_pct) AS commission
+FROM employees
+WHERE commission_pct IS NOT NULL;
+
+
+
+
+
+
--- scott1104.sql
+++ scott1104.sql
@@ -91,3 +91,86 @@ SELECT * FROM emp WHERE job NOT IN('MANAGER', 'SALESMAN', 'CLERK'); --() 모두에 
 --IN 을 사용하여 부서번호가 10번, 20번인 사원 정보 조회
 SELECT * FROM emp WHERE deptno IN(10, 20);
 
+--BETWEEN A AND B : A~B의 범위 표현
+
+--1) 급여 2000이상 3000 이하인 사원 정보
+SELECT * FROM emp WHERE sal >=2000 AND sal <=3000;
+SELECT * FROM emp WHERE sal BETWEEN 2000 AND 3000 ORDER BY sal;
+
+--1) 급여 2000이상 3000 이하가 아닌 사원 정보
+SELECT * FROM emp WHERE sal NOT BETWEEN 2000 AND 3000 ORDER BY sal;
+
+--LIKE : 문자열 검색 (ex. s로 시작하는 ~~, H로 끝나는..., 가운데에 T가 있으면)
+
+--1) 사원 이름이 S로 시작하는 모든 사원정보 조회하기
+SELECT * FROM emp WHERE ename LIKE 'S%';
+
+--2) 사원 이름의 두번째 글자가 L인 사원만 조회
+SELECT * FROM emp WHERE ename LIKE '_L%';
+
+--3) 사원 이름에 AM이 포함되어 있는 사원만 조회
+SELECT * FROM emp WHERE ename LIKE '%AM%';
+
+--4) 사원 이름에 AM이 포함되어 있지 않는 사원만 조회
+SELECT * FROM emp WHERE ename NOT LIKE '%AM%';
+
+--IS NULL
+--SELECT * FROM EMP WHERE COMM = NULL; (x)
+SELECT * FROM emp WHERE comm IS NULL;
+
+--MGR(매니저 ID)이 NULL인 사원정보 조회
+SELECT * FROM emp WHERE mgr IS NULL;
+--MGR(매니저 ID)이 NULL이 아닌 사원정보 조회
+SELECT * FROM emp WHERE mgr IS NOT NULL;
+
+desc 
+
+SELECT TO_DATE('2020-11-05', 'YYYY/MM/DD') AS TOTATE1,
+              TO_DATE('20201105', 'YYYY-MM-DD') AS TOTATE2 FROM DUAL;
+    
+--1981년 6월 1일 이후에 입사한 사원 정보 조회
+desc emp;
+select *
+from emp
+where hiredate > to_date('1981-06-01', 'YYYY-MM-DD');
+
+--to_date, to_number : 연산 시 오류 최소화
+SELECT TO_DATE('2019-12-20') - TO_DATE('2019-10-20') FROM dual;
+
+--NULL 처리 함수 : null일때 수행할 동작 구현 / NVL, NVL2
+--NVL(A, a) : A가 null일때 a 수행
+--NVL(A, a, b) : A가 null이 아닐 때 a 수행 / null일 때 b 수행
+SELECT empno, ename, sal, comm, sal+comm FROM emp;
+SELECT empno, ename, sal, comm, sal+comm, nvl(comm, 0), sal+nvl(comm, 0)
+FROM emp;
+SELECT empno, ename, sal, comm, nvl2(comm, 'O', 'X'), 
+                nvl2(comm, SAL*12+COMM, SAL*12) AS ANNSAL
+FROM emp;
+
+--DECODE함수와 CASE문
+----job이 manager, salesman, analyst 경우 각각 다른 비율 적용
+SELECT empno, ename, JOB, sal, DECODE(JOB,
+                                                                    'manager', sal*1.1,
+                                                                    'salesman', sal*1.05,
+                                                                    'analyst', sal,
+                                                                    sal*1.03) AS UPSAL 
+FROM emp;
+
+SELECT empno, ename, JOB, sal, CASE JOB
+                                                     WHEN 'manager' THEN sal*1.1,
+                                                     WHEN'salesman' THEN sal*1.05,
+                                                     WHEN 'analyst' THEN sal,
+                                                     ELSE sal*1.03
+                                                END AS UPSAL 
+FROM emp;
+
+SELECT empno, ename, job, sal CASE
+                                                        WHEN comm IS NULL THEN '해당사항 없음'
+                                                        WHEN comm = 0 THEN '수당 없음'
+                                                        WHEN comm > 0 THEN '수당 : ' ||COMM
+                                                    END AS COMM_TEXT FROM EMP;
+
+
+
+
+
